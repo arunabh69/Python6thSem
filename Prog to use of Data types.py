dd9b40353cb98ee54662b07c449972ede44371b0
arunabh69 <57730633+arunabh69@users.noreply.github.com>
2022-03-03 16:25:00 +0530

    Create Prog to use of Data types.py

---
+++ Prog to use of Data types.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    print("sum of two numbers", a + b)
+    print("remainder when a divides b", b%a)
+    n = int(input())
+    strz = str(n)
+    print("sum of first and third digit of integer", int(strz[-0])+int(strz[-2]))
