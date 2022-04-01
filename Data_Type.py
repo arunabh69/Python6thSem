b8628d6d4498002e67b1d03e8133d6232f233b09
arunabh69 <57730633+arunabh69@users.noreply.github.com>
2022-04-01 14:42:00 +0530

    Create Data_Type.py

---
+++ Data_Type.py
@@ -0,0 +1,34 @@
+i=22
+print(type(i))
+
+i=22.1
+print(type(i))
+
+i=True
+print(type(i))
+
+i=1+22j
+print(type(i))
+
+i=[1,2,3,4]
+print(type(i))
+
+i=(1,2,3,4)
+print(type(i))
+
+i={1,3,7,8,9}
+print(type(i))
+
+i={"Name" : "Akshat" ,"age":20}
+print(type(i))
+
+Ans:-
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'bool'>
+<class 'complex'>
+<class 'list'>
+<class 'tuple'>
+<class 'set'>
+<class 'dict'>
